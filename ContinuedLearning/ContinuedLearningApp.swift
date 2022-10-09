0413ff57d23b26dd16500e10e113c87bc33665eb
LeeTaehyung <thlee@kb-sys.com>
2022-10-09 13:25:42 +0900

    Initial Commit

---
+++ ContinuedLearning/ContinuedLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContinuedLearningApp.swift
+//  ContinuedLearning
+//
+//  Created by Taehyung Lee on 2022/10/09.
+//
+
+import SwiftUI
+
+@main
+struct ContinuedLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
